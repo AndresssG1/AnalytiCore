bdf160bb4f915037e9b24fd8e325f1f81294aedb
AndresssG1 <jachumo@espe.edu.ec>
2025-08-05 02:45:35 -0500

    tercer commit

--- python-service/db.js
+++ python-service/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "postgres", // nombre del contenedor del servicio en docker-compose
-  database: "analyti_core",
-  password: "postgres",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Necesario para Render
+  },
 });
 
 module.exports = pool;
